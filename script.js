8b721158ce1bb5a52970e358df73a932e95a3269
Ritesh Kishor Chaudhari <100477678+Ritu0097@users.noreply.github.com>
2024-02-25 19:28:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function getFormvalue() {
 	let input1=document.getElementById('form1').elements["fname"].value;
     let input2=document.getElementById('form1').elements["lname"].value;
-    alert(input1 + input2);
+    alert(input1 +' '+ input2);
 }
